74ac468b3585e563a287e2f750016c781bad977b
goddie <goddie2046@gmail.com>
2015-04-28 18:00:01 +0800

    no message

---
+++ PhoneGame/GameFramwork/Manager/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GameFramwork
+{
+	public class AudioManager
+	{
+		private static AudioManager instance = null;
+
+		public static AudioManager SharedInstance {
+			get {
+				if (instance == null) {
+					instance = MainComponentManager.AddMainComponent<AudioManager> ();
+				}
+				return instance;
+			}
+		}
+	}
+}
+
